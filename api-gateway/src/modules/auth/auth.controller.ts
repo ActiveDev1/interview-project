ccdf7486d81388eb140df44d3ac4410be30efc74
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-08-07 17:39:35 +0430

    chore(Controllers): implement swagge docs

--- api-gateway/src/modules/auth/auth.controller.ts
+++ api-gateway/src/modules/auth/auth.controller.ts
@@ -1,20 +1,30 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common'
-import { Role } from 'src/shared/decorators/roles.decorator'
-import { AuthGuard } from 'src/shared/guards/auth.guard'
-import { RolesGuard } from 'src/shared/guards/roles.guard'
+import {
+	ApiBearerAuth,
+	ApiCreatedResponse,
+	ApiNotAcceptableResponse,
+	ApiTags
+} from '@nestjs/swagger'
+import { Role } from '../../shared/decorators/roles.decorator'
+import { Role as Roles } from '../../shared/enums/role.enum'
+import { AuthGuard } from '../../shared/guards/auth.guard'
+import { RolesGuard } from '../../shared/guards/roles.guard'
 import { NatsService as NatsClient } from '../services/nats/nats.service'
 import { CreateUserDto } from '../user/dto/create-user.dto'
 import { LoginDto } from './dto/login.dto'
 import { Tokens } from './interfaces/token.interface'
-import { Role as Roles } from '../../shared/enums/role.enum'
 
 @Controller('auth')
+@ApiTags('Auth')
 export class AuthController {
 	constructor(private readonly client: NatsClient) {}
 
 	@Post('signup/admin')
 	@UseGuards(AuthGuard(), RolesGuard)
 	@Role(Roles.ADMIN)
+	@ApiBearerAuth('access-token')
+	@ApiCreatedResponse({ description: 'Admin created', type: Tokens })
+	@ApiNotAcceptableResponse({ description: 'Admin exists' })
 	async signupAdmin(@Body() body: CreateUserDto): Promise<Tokens> {
 		return await this.client.request<Tokens, CreateUserDto & { role: Roles }>({
 			action: 'auth.signup',
