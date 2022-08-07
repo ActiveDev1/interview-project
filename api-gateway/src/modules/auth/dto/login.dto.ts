9126e688a01da03b79ff5cd14cceef97e2a901f2
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-08-07 17:36:56 +0430

    chore(Dtos): add api property

--- api-gateway/src/modules/auth/dto/login.dto.ts
+++ api-gateway/src/modules/auth/dto/login.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsNotEmpty, IsString, Length } from 'class-validator'
 
 export class LoginDto {
+	@ApiProperty()
 	@IsString()
 	@IsNotEmpty()
 	@Length(3, 20)
 	username: string
 
+	@ApiProperty()
 	@IsString()
 	@IsNotEmpty()
 	@Length(6, 30)
--- api-gateway/src/modules/user/dto/create-user.dto.ts
+++ api-gateway/src/modules/user/dto/create-user.dto.ts
@@ -1,16 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsNotEmpty, IsString, Length } from 'class-validator'
 
 export class CreateUserDto {
+	@ApiProperty()
 	@IsString()
 	@IsNotEmpty()
 	@Length(3, 45)
 	name: string
 
+	@ApiProperty()
 	@IsString()
 	@IsNotEmpty()
 	@Length(3, 20)
 	username: string
 
+	@ApiProperty()
 	@IsString()
 	@IsNotEmpty()
 	@Length(6, 30)
