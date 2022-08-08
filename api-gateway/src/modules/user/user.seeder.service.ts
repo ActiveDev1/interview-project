05cb5ed10d7484c49a92d15fd02ca7856d122406
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-08-09 03:58:30 +0430

    fix(Seeder): check admin exists before insert

--- api-gateway/src/modules/user/user.seeder.service.ts
+++ api-gateway/src/modules/user/user.seeder.service.ts
@@ -10,10 +10,13 @@ export class UsersSeeder implements Seeder {
 	constructor(@InjectModel(User.name) private readonly user: Model<User>) {}
 
 	async seed(): Promise<any> {
-		const users = DataFactory.createForClass(User).generate(1)
-		this.logger.log(`Password is ${users[0]?.password}`)
+		const admin = await this.user.findOne({ username: 'Admin' })
+		if (!admin) {
+			const users = DataFactory.createForClass(User).generate(1)
+			this.logger.log(`Password is ${users[0]?.password}`)
 
-		return this.user.insertMany(users)
+			return this.user.insertMany(users)
+		}
 	}
 
 	async drop(): Promise<any> {
