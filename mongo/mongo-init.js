71d5d14bf1a55d14c53db93fc7b9c7f157b7aec2
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-08-09 17:53:23 +0430

    refactor mongo-init user roles

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -2,5 +2,5 @@ db = db.getSiblingDB('admin')
 db.createUser({
     user: 'admin',
     pwd: '0!PW*o9TYmnBm9MdUtwKw7AL',
-    roles: [{ role: 'dbOwner', db: 'UserManagement' }]
+    roles: ['root']
 })
