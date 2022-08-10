8d5b09155b4e3568eac279024eda12d81010b716
ActiveDev <sarbazmahdi.2014@gmail.com>
2022-08-10 19:38:16 +0430

    refactor(Modules): remove unused module

--- api-gateway/src/modules/auth/auth.module.ts
+++ api-gateway/src/modules/auth/auth.module.ts
@@ -1,24 +1,11 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import { JwtModule } from '@nestjs/jwt'
 import { NatsClientModule } from '../services/nats/nats.module'
 import { NatsService } from '../services/nats/nats.service'
 import { UserModule } from '../user/user.module'
 import { AuthController } from './auth.controller'
 
 @Module({
-	imports: [
-		UserModule,
-		NatsClientModule,
-		JwtModule.registerAsync({
-			imports: [ConfigModule],
-			useFactory: (config: ConfigService) => ({
-				secret: config.get<string>('jwt.access.secret'),
-				signOptions: { expiresIn: config.get<string | number>('jwt.access.expiresIn') }
-			}),
-			inject: [ConfigService]
-		})
-	],
+	imports: [UserModule, NatsClientModule],
 	controllers: [AuthController],
 	providers: [NatsService]
 })
